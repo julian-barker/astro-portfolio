fa2c09986b09ca23c00e17e9ff4c4af602a49fd9
Julian Barker <julian.c.barker@gmail.com>
2022-10-08 17:33:27 -0700

    add layouts, color scheme, hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'sans': ['"Fira Code"', ...defaultTheme.fontFamily.sans],
+				'audiowide': ['Audiowide', ...defaultTheme.fontFamily.sans],
+				'fira': ['"Fira Code"', ...defaultTheme.fontFamily.mono],
+				'mono': ['"Major Mono Display"', ...defaultTheme.fontFamily.mono],
+				'special': ['"Special Elite"', ...defaultTheme.fontFamily.sans]
+			},
+		},
 	},
 	plugins: [],
 }
